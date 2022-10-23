5aea2f3695e36acf5f05b422f5c19be495218762
cheung0 <cheung0-bit@qq.com>
2022-10-23 22:55:42 +0800

    great foreshadowing

---
+++ pojo/Topsis.py
@@ -0,0 +1,14 @@
+class Topsis:
+  lang_var = ''
+  tfn = ()
+  
+  def __init__(self, lang_var, tfn):
+    self.lang_var = lang_var
+    self.tfn = tfn
+  
+TOPSIS_TABLE = []
+TOPSIS_TABLE.append(Topsis('非常差',(1,2,3)))
+TOPSIS_TABLE.append(Topsis('差',(2,3,4)))
+TOPSIS_TABLE.append(Topsis('一般',(3,4,5)))
+TOPSIS_TABLE.append(Topsis('好',(4,5,6)))
+TOPSIS_TABLE.append(Topsis('非常好',(5,7,9)))
