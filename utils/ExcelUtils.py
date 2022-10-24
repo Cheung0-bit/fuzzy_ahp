8d0b7333c420b6670814a9cdd78371236f8f080d
pixel-revolve <pixel-revolve@qq.com>
2022-10-24 16:07:22 +0800

    完成excel表格映射

--- utils/ExcelUtils.py
+++ utils/ExcelUtils.py
@@ -1,14 +1,7 @@
 import xlrd
-from model.FactorRes import FactorRes
-
-
-def bracketsSoup(str):
-    startIndex = str.find('(') + 1
-    endIndex = str.find(')')
-    if endIndex == -1:
-        return ''
-    else:
-        return str[startIndex:endIndex]
+from utils.StringUtils import bracketsSoup
+from model.FactorFeedback import FactorFeedback
+from model.SchemeFeedback import SchemeFeedback
 
 
 book = xlrd.open_workbook('../resources/data.xlsx')
@@ -17,8 +10,25 @@ factorSheet = book.sheet_by_name('因素反馈')
 for i in range(factorSheet.nrows):
     row = factorSheet.row_values(i)
     factor = bracketsSoup(row[0])
+    res = []
     if factor != '':
-        print(factor)
-        FactorRes()
-    # print(row)
-    # print(row[0])
+        res.append([factor, row[1:]])
+        FactorFeedback.resList.append(res)
+
+schemeSheet = book.sheet_by_name('方案反馈')
+scheme = ''
+for i in range(schemeSheet.nrows):
+    row = schemeSheet.row_values(i)
+    if row[0] != '':
+        scheme = row[0]
+    factor = bracketsSoup(row[1])
+    res = []
+    if factor != '':
+        res.append([scheme, factor, row[1:]])
+        SchemeFeedback.resList.append(res)
+
+for item in FactorFeedback.resList:
+    print(item)
+
+for item in SchemeFeedback.resList:
+    print(item)
